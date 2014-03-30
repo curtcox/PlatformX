d88848d2c2ba1a3f777e7ab1d78750bea880b831
Curt Cox <curtcox@gmail.com>
2014-03-29 22:25:08 -0500

    Use 5 star ratings with explanations and adjust layout

--- OC1/src/com/mycompany/myapp/screens/RateScreen.java
+++ OC1/src/com/mycompany/myapp/screens/RateScreen.java
@@ -2,14 +2,17 @@ package com.mycompany.myapp.screens;
 
 import com.codename1.ui.Button;
 import com.codename1.ui.Component;
+import com.codename1.ui.Container;
 import com.codename1.ui.Label;
 import com.codename1.ui.layouts.GridLayout;
+import com.codename1.ui.table.TableLayout;
 import com.mycompany.myapp.domain.CurrentState;
 import com.mycompany.myapp.domain.Rating;
 import com.mycompany.myapp.ui.ActionButton;
 
 /**
- *
+ * See
+ * http://www.sagetraveling.com/Rating-System-Explanation/
  * @author Curt
  */
 final class RateScreen
@@ -25,15 +28,35 @@ final class RateScreen
     }
 
     private void addComponents() {
-        form.setLayout(new GridLayout(8,1));
-        form.addComponent(provider);
+        form.setLayout(new TableLayout(4,1));
+        form.addComponent(newProviderSummary());
+        form.addComponent(new Label("Rating : Suitable for"));
+        form.addComponent(newRatingTable());
         form.addComponent(newChangeLocationButton());
-        form.addComponent(new Label("Rating"));
-        form.addComponent(rating);
-        form.addComponent(newStarButton("*"));
-        form.addComponent(newStarButton("**"));
-        form.addComponent(newStarButton("***"));
-        form.addComponent(newStarButton("****"));
+    }
+
+    private Component newProviderSummary() {
+        Container container = new Container();
+        container.setLayout(new GridLayout(1,2));
+        container.addComponent(provider);
+        container.addComponent(rating);
+        return container;
+    }
+    
+    private Container newRatingTable() {
+        Container table = new Container();
+        table.setLayout(new TableLayout(5,2));
+        addRatingAndInfo(table,"*",    "people who can walk up a flight of stairs");
+        addRatingAndInfo(table,"**",   "slow walkers or wheelchair users who can get up a few steps");
+        addRatingAndInfo(table,"***",  "wheelchair users with full use of upper body (paraplegics)");
+        addRatingAndInfo(table,"****", "wheelchair users with limited arm/hand use");
+        addRatingAndInfo(table,"*****","wheelchair users with no arm/hand use (quadriplegics)");
+        return table;
+    }
+    
+    private void addRatingAndInfo(Container table, String rating, String description) {
+        table.addComponent(newStarButton(rating));
+        table.addComponent(new Label(description));
     }
     
     private Button newStarButton(final String text) {
