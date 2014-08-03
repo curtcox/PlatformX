61563ee2dfd6dafc002e19de3527a110a17676ad
Curt Cox <curtcox@gmail.com>
2014-08-03 18:07:52 -0500

    Added checkboxes for provider types on filter screen

---
+++ OC1/src/oc1/ui/BoxYContainer.java
@@ -0,0 +1,19 @@
+package oc1.ui;
+
+import com.codename1.ui.Component;
+import com.codename1.ui.Container;
+import com.codename1.ui.layouts.BoxLayout;
+
+/**
+ * @author curt
+ */
+public final class BoxYContainer
+    extends Container
+{
+    public BoxYContainer(Component... components) {
+        setLayout(new BoxLayout(BoxLayout.Y_AXIS));
+        for (Component component : components) {
+            addComponent(component);
+        }
+    }
+}
