3decfa279d575911d1af963a59d8921ef053c8bc
Curt Cox <curtcox@gmail.com>
2014-08-03 19:26:59 -0500

    Rev version

--- OC1/src/oc1/screens/HomeScreen.java
+++ OC1/src/oc1/screens/HomeScreen.java
@@ -22,7 +22,7 @@ public final class HomeScreen
     extends Screen
 {
    
-    public static final String VERSION = "Oyster Cracker 2014/8/3 7:43a";
+    public static final String VERSION = "Oyster Cracker 2014/8/3 7:26p";
     
     private HomeScreen() {
         super(VERSION,null);
