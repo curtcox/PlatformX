aa2d52a3da42be2ff2688a6afdd0ca272bf5d2cd
Curt Cox <curtcox@gmail.com>
2014-10-02 22:11:59 -0500

    Created failing test for SimpleNetStringMap

---
+++ OC1/src/oc2/net/SimpleNetStringMap.java
@@ -0,0 +1,22 @@
+package oc2.net;
+
+import java.net.URI;
+import oc1.net.Network;
+import oc1.util.StringMap;
+
+/**
+ *
+ * @author Curt
+ */
+public final class SimpleNetStringMap
+    implements StringMap
+{
+
+    SimpleNetStringMap(URI base, Network network) {
+    }
+
+    public String get(String string) {
+        return "";
+    }
+    
+}
---
+++ OC1/test/oc2/net/SimpleNetStringMapTest.java
@@ -0,0 +1,59 @@
+package oc2.net;
+
+import com.codename1.ui.Image;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import oc1.net.Network;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author Curt
+ */
+public class SimpleNetStringMapTest {
+
+    Map<URI,String> pages = new HashMap<URI,String>();
+    
+    Network network = new Network() {
+        public InputStream getStreamFor(URI uri) {
+            return new ByteArrayInputStream(pages.get(uri).getBytes());
+        }
+        public Image getImage(URI uri) { throw new UnsupportedOperationException(""); }
+    };
+    
+    URI base = URI("http://example.com/");
+    URI valuePage = URI("http://example.com/Vodka");
+    String key = "Vodka";
+    String value = "0 Kelvin";
+    String relativeValue = "warmish";
+    SimpleNetStringMap testObject = new SimpleNetStringMap(base,network);
+    
+    @Before
+    public void setUp() {
+        pages.put(valuePage, value);
+        pages.put(URI("http://example.com/relative_value_page"), relativeValue);
+    }
+    
+    @Test
+    public void get_uses_network_to_return_value_from_key() {
+        String expected = value;
+        
+        String actual = testObject.get(key);
+        
+        assertEquals(expected,actual);
+    }
+
+    private URI URI(String string) {
+        try {
+            return new URI(string);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+}
