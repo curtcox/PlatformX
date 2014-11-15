16a54cbc2ba3a3eb0a8b3955de5c87ff7fe5cbbe
Curt Cox <curtcox@gmail.com>
2014-11-15 13:53:21 -0600

    Implemented SyntaxError

--- OC1/src/hash/SyntaxError.java
+++ OC1/src/hash/SyntaxError.java
@@ -16,11 +16,35 @@ public final class SyntaxError
         DUPLICATE_METHOD_NAME
     }
 
+    final String methodSource;
+    final String errorSource;
+    final Type type;
+    
     public SyntaxError(String methodSource, String errorSource, Type type) {
+        this.errorSource = errorSource;
+        this.methodSource = methodSource;
+        this.type = type;
     }
 
     public Object invokeIn(Context context) {
-        return null;
+        return this;
     }
 
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        SyntaxError that = (SyntaxError) o;
+        return methodSource.equals(that.methodSource) &&
+               errorSource.equals(that.errorSource) &&
+               type.equals(that.type);
+    }
+    
+    @Override
+    public String toString() {
+        return methodSource + " " + errorSource + " " + type;
+    }
 }
---
+++ OC1/test/hash/SyntaxErrorTest.java
@@ -0,0 +1,45 @@
+package hash;
+
+import oc1.util.Strings;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static hash.SyntaxError.Type.*;
+
+public class SyntaxErrorTest {
+
+    @Test
+    public void equals_returns_true_for_params_with_the_same_values() {
+        assertEquals(new SyntaxError("a","b",DUPLICATE_METHOD_NAME),new SyntaxError("a","b",DUPLICATE_METHOD_NAME));
+    }
+
+    @Test
+    public void equals_returns_false_for_params_with_different_values() {
+        assertNotEquals(new SyntaxError("a","b",DUPLICATE_METHOD_NAME),new SyntaxError("a","a",DUPLICATE_METHOD_NAME));
+        assertNotEquals(new SyntaxError("a","a",DUPLICATE_METHOD_NAME),new SyntaxError("a","a",INVALID_METHOD_NAME));
+    }
+
+    private void assertEquals(SyntaxError a, SyntaxError b) {
+        assertTrue(a.equals(b));
+        assertTrue(b.equals(a));
+        assertTrue(a.hashCode()==b.hashCode());
+    }
+
+    private void assertNotEquals(SyntaxError a, SyntaxError b) {
+        assertFalse(a.equals(b));
+        assertFalse(b.equals(a));
+    }
+    
+    @Test
+    public void toString_contains_params() {
+        SyntaxError error = new SyntaxError("nuts","bolts",DUPLICATE_METHOD_NAME);
+        assertTrue(Strings.contains(error.toString(),"nuts"));
+        assertTrue(Strings.contains(error.toString(),"bolts"));
+        assertTrue(Strings.contains(error.toString(),DUPLICATE_METHOD_NAME.toString()));
+    }
+
+    @Test
+    public void invoke_returns_error_itself() {
+        SyntaxError error = new SyntaxError("nuts","bolts",DUPLICATE_METHOD_NAME);
+        assertSame(error,error.invokeIn(null));
+    }
+}
