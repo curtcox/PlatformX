b84900fb26d182e3284444dfd294ed7378ab7fdc
Curt Cox <curtcox@gmail.com>
2014-11-27 08:04:43 -0600

    Added docs

--- OC1/src/oc1/util/StringMap.java
+++ OC1/src/oc1/util/StringMap.java
@@ -1,18 +1,20 @@
 package oc1.util;
 
-import java.util.HashMap;
-
 /**
  * A map from strings to strings.
+ * This is a read-only abstraction that could be backed by for a HashMap,
+ * a file, a file system, a remote server, or some combination of the 
+ * previous.
  * @author Curt
  */
 public interface StringMap {
 
+    /**
+     * For parsing a String into a StringMap.
+     */
     interface Parser {
         StringMap parse(String string);    
     }
     
     String get(String string);
-    
-    static final StringMap EMPTY = new SimpleStringMap(new HashMap());
 }
