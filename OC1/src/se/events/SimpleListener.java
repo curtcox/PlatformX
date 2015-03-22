28bffd23c057cdc3e362a0694009b553ed343591
Curt Cox <curtcox@gmail.com>
2015-03-21 22:02:28 -0500

    Implemented SimpleListener

--- OC1/src/se/events/SimpleListener.java
+++ OC1/src/se/events/SimpleListener.java
@@ -3,12 +3,14 @@ package se.events;
 public final class SimpleListener
     implements Events.Listener
 {
+    private Events.Event event;
+
     @Override
     public void onEvent(Events.Event event) {
-
+        this.event = event;
     }
 
     public Events.Event getLast() {
-        return null;
+        return event;
     }
 }
