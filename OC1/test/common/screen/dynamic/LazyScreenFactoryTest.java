cff6dd0a17d2cac2dec519b0e66002c4b8f0f240
Curt Cox <curtcox@gmail.com>
2015-04-18 13:21:13 -0500

    Fixed test.

--- OC1/test/common/screen/dynamic/LazyScreenFactoryTest.java
+++ OC1/test/common/screen/dynamic/LazyScreenFactoryTest.java
@@ -1,14 +1,16 @@
 package common.screen.dynamic;
 
+import common.event.StringSource;
 import common.screen.Screen;
 import common.screen.ScreenLink;
-import common.util.StringMap;
+import common.screen.ScreenTags;
 import fake.FakeSERegistryLoader;
 import junit.framework.TestCase;
 import mach.Mocks;
 import org.junit.Before;
 import org.junit.Test;
 
+import static mach.Mocks._;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -33,6 +35,7 @@ public class LazyScreenFactoryTest {
     @Test
     public void create_returns_empty_array_when_there_are_no_sources() {
         ScreenLink link = ScreenLink.of("");
+        _(new StringSource[0]); sources.get(ScreenTags.of(""));
 
         Screen[] actual = testObject.create(link);
 
