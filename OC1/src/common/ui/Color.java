6ca4240791ca08cb8467e0bc381a7778ee74ae58
Curt Cox <curtcox@gmail.com>
2015-05-05 21:26:19 -0500

    Added colors red, green, blue

--- OC1/src/common/ui/Color.java
+++ OC1/src/common/ui/Color.java
@@ -2,8 +2,9 @@ package common.ui;
 
 public final class Color {
 
-    public static final Color RED = null;
-    public static final Color BLUE = null;
+    public static final Color RED   = new Color(0xff0000);
+    public static final Color GREEN = new Color(0x00ff00);
+    public static final Color BLUE  = new Color(0x0000ff);
     public static final Color BLACK = new Color(0x000000);
     public static final Color WHITE = new Color(0xffffff);
 
