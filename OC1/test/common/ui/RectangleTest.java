703ddacadd11edf3e894d1a6e26a90399792a6e5
Curt Cox <curtcox@gmail.com>
2015-05-06 18:04:26 -0500

    Make Rectangle.contains work just like java.awt counterpart.

--- OC1/test/common/ui/RectangleTest.java
+++ OC1/test/common/ui/RectangleTest.java
@@ -1,6 +1,5 @@
 package common.ui;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -10,35 +9,58 @@ public class RectangleTest {
 
     @Test
     public void rectangle_0_0_10_10_contains() {
-        Rectangle testObject = new Rectangle(0,0,10,10);
+        Rectangle rect = new Rectangle(0,0,10,10);
 
-        assertTrue(testObject.contains(new Point(1,1)));
-        assertTrue(testObject.contains(new Point(1,9)));
-        assertTrue(testObject.contains(new Point(9,1)));
+        contains(rect, 1, 1);
+        contains(rect, 1, 9);
+        contains(rect, 9, 1);
     }
 
     @Test
     public void rectangle_0_0_0_0_does_not_contain_anything() {
-        Rectangle testObject = new Rectangle(0,0,0,0);
+        Rectangle rect = new Rectangle(0,0,0,0);
 
-        assertFalse(testObject.contains(new Point(0, 0)));
+        doesNotContain(rect, 0, 0);
     }
 
     @Test
     public void rectangle_0_0_2_2_contains_1_1() {
-        Rectangle testObject = new Rectangle(0,0,2,2);
-
-        assertTrue(testObject.contains(new Point(1, 1)));
+        Rectangle rect = new Rectangle(0,0,2,2);
+        contains(rect, 0, 0);
+        contains(rect, 1, 1);
+        contains(rect, 0, 1);
+        contains(rect, 1, 0);
+        doesNotContain(rect, 2, 2);
     }
 
     @Test
     public void rectangle_0_0_10_10_does_not_contain() {
-        Rectangle testObject = new Rectangle(0,0,10,10);
+        Rectangle rect = new Rectangle(0,0,10,10);
 
-        assertFalse(testObject.contains(new Point(1, -1)));
-        assertFalse(testObject.contains(new Point(-1, 1)));
-        assertFalse(testObject.contains(new Point(1, 11)));
-        assertFalse(testObject.contains(new Point(11, 1)));
+        doesNotContain(rect, 1, -1);
+        doesNotContain(rect, -1, 1);
+        doesNotContain(rect, 1, 11);
+        doesNotContain(rect, 11, 1);
+    }
+
+    private void contains(Rectangle rectangle, int x, int y) {
+        Point point = new Point(x,y);
+        assertTrue(rectangle.contains(point));
+        assertTrue(awt(rectangle).contains(awt(point)));
+    }
+
+    private void doesNotContain(Rectangle rectangle, int x, int y) {
+        Point point = new Point(x,y);
+        assertFalse(rectangle.contains(point));
+        assertFalse(awt(rectangle).contains(awt(point)));
+    }
+
+    private java.awt.Point awt(Point point) {
+        return new java.awt.Point(point.x,point.y);
+    }
+
+    private java.awt.Rectangle awt(Rectangle rect) {
+        return new java.awt.Rectangle(rect.x,rect.y,rect.w,rect.h);
     }
 
 }
