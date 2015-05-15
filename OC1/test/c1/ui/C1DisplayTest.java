0c871413adb5bacb78ec10fe5b69ee4d5ad8a14d
Curt Cox <curtcox@gmail.com>
2015-05-14 21:52:04 -0500

    Added delay to make tests more reliable

--- OC1/test/c1/ui/C1DisplayTest.java
+++ OC1/test/c1/ui/C1DisplayTest.java
@@ -18,23 +18,25 @@ public class C1DisplayTest {
     }
 
     @Test
-    public void getCurrent_returns_IForm_when_IForm() {
+    public void getCurrent_returns_IForm_when_IForm() throws InterruptedException {
         Display.getInstance();
         FakeUIManager.of();
         C1Display display = new C1Display();
         new C1Form(ScreenLink.of("")).show();
 
+        Thread.sleep(50);
         assertNotNull(display.getCurrent());
         assertTrue(display.getCurrent() instanceof IForm);
     }
 
     @Test
-    public void getCurrent_returns_null_when_not_IForm() {
+    public void getCurrent_returns_null_when_not_IForm() throws InterruptedException {
         Display.getInstance();
         FakeUIManager.of();
         C1Display display = new C1Display();
         new Form().show();
 
+        Thread.sleep(50);
         assertNull(display.getCurrent());
         assertFalse(display.getCurrent() instanceof IForm);
     }
