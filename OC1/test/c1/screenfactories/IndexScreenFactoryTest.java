0b9ab7bd9a2ae1d4580dc492d111cd165a06ceb1
Curt Cox <curtcox@gmail.com>
2015-05-16 10:47:05 -0500

    Added tests

--- OC1/test/c1/screenfactories/IndexScreenFactoryTest.java
+++ OC1/test/c1/screenfactories/IndexScreenFactoryTest.java
@@ -4,7 +4,10 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class IndexScreenFactoryTest {
@@ -13,4 +16,19 @@ public class IndexScreenFactoryTest {
     public void can_create() {
         assertNotNull(new IndexScreenFactory(new ArrayList<String>()));
     }
+
+    @Test
+    public void empty_index_has_no_values() {
+        assertEquals(0, new IndexScreenFactory(new ArrayList<String>()).getValues().size());
+    }
+
+    @Test
+    public void index_with_1_value() {
+        String value = "whatever";
+        IndexScreenFactory testObject = new IndexScreenFactory(Arrays.asList(new String[]{value}));
+        List<String> values = testObject.getValues();
+        assertEquals(1, values.size());
+        assertEquals(value, values.get(0));
+    }
+
 }
