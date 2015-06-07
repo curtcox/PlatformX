187248253932d830341518f0a1ddb507d7aedbff
Curt Cox <curtcox@gmail.com>
2015-06-07 15:09:46 -0500

    Added system properties to SE device info

--- OC1/src/se/device/SEDeviceInfo.java
+++ OC1/src/se/device/SEDeviceInfo.java
@@ -5,6 +5,8 @@ import common.device.IDeviceInfo;
 import common.device.ReportBuilder;
 
 import java.util.List;
+import java.util.Properties;
+import java.util.TreeSet;
 
 /**
  * For generating a dump of device-specific info.
@@ -24,14 +26,25 @@ public final class SEDeviceInfo
     static ReportBuilder buildReport() {
         ReportBuilder out = new ReportBuilder();
         out.section("Runtime",runtimeInfo());
+        out.section("System",systemInfo());
         return out;
     }
 
     private static List<DeviceKeyValuePair> runtimeInfo() {
         Runtime runtime = Runtime.getRuntime();
         ReportBuilder out = new ReportBuilder();
-        out.value("free memory"  , runtime.freeMemory());
+        out.value("free memory", runtime.freeMemory());
         out.value("total memory" , runtime.totalMemory());
+        out.value("max memory", runtime.maxMemory());
+        return out.toKeyValuePairs();
+    }
+
+    private static List<DeviceKeyValuePair> systemInfo() {
+        Properties properties = System.getProperties();
+        ReportBuilder out = new ReportBuilder();
+        for (Object key : new TreeSet(properties.keySet())) {
+            out.value(key.toString(), properties.get(key));
+        }
         return out.toKeyValuePairs();
     }
 
