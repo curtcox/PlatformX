fb7fa93e4f75f5ca13fd4f61d2b093e8b67de2a5
Curt Cox <curtcox@gmail.com>
2015-06-14 22:17:51 -0500

    Start of Android port. Added android.jar and Hello World Activity.

---
+++ OC1/src/an/a22/FullscreenActivity.java
@@ -0,0 +1,31 @@
+package an.a22;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class FullscreenActivity extends Activity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView label = new TextView(this);
+        label.setText("Hello World");
+        label.setTextSize(20);
+        label.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
+        layout.setGravity(Gravity.CENTER);
+        layout.addView(label);
+
+        setContentView(layout);
+    }
+
+}
