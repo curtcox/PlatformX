5a363978b6fd18c65353766c02fa978c93aaf3ed
Curt Cox <curtcox@gmail.com>
2015-06-15 21:39:48 -0500

    Added Robolectric to write Android tests

--- OC1/test/an/a22/app/AnApplicationTest.java
+++ OC1/test/an/a22/app/AnApplicationTest.java
@@ -1,13 +1,15 @@
 package an.a22.app;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.assertNotNull;
 
+@RunWith(RobolectricTestRunner.class)
 public class AnApplicationTest {
 
-    @Test @Ignore // Add Robolectric?
+    @Test
     public void can_create() {
         assertNotNull(new AnApplication());
     }
