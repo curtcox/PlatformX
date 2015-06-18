7d3d6c5d5ef139bcfced5cc4870494ea7d9cbdc9
Curt Cox <curtcox@gmail.com>
2015-06-18 17:21:13 -0500

    Implement enough BorderContainer to make tests pass.

--- OC1/src/an/a22/uiwidget/AnBorderContainer.java
+++ OC1/src/an/a22/uiwidget/AnBorderContainer.java
@@ -2,16 +2,22 @@ package an.a22.uiwidget;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.RelativeLayout;
+import common.Registry;
 
 public final class AnBorderContainer
-    extends View
+    extends RelativeLayout
 {
     private AnBorderContainer(Context context) {
         super(context);
     }
 
     public static AnBorderContainer of(View view) {
-        return null;
+        return new AnBorderContainer(context());
+    }
+
+    private static Context context() {
+        return Registry.get(Context.class);
     }
 
     public AnBorderContainer addNorth(View component) {
