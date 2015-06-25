b42d4ecddab1a6c0d4ffec3dd29d56c4abe9f47c
Curt Cox <curtcox@gmail.com>
2015-06-24 19:45:27 -0500

    Added tests

--- OC1/test/fake/FakeSERegistryLoader.java
+++ OC1/test/fake/FakeSERegistryLoader.java
@@ -1,21 +1,24 @@
 package fake;
 
+import se.pagefactories.SEItemListPageFactoryFactory;
 import x.Registry;
 import x.domain.ServiceProvider;
+import x.log.ILogManager;
 import x.log.XLogManager;
 import x.log.XLogWriter;
-import x.log.ILogManager;
+import x.pagefactories.ItemListPageFactoryFactory;
 import x.ui.IDisplay;
 import x.ui.IFormFactory;
 
 public class FakeSERegistryLoader {
     
     public static void load() {
-        put(ILogManager.class,      new XLogManager());
-        put(XLogWriter.class,  new XLogWriter());
-        put(ServiceProvider.class,  ServiceProvider.NULL);
-        put(IDisplay.class,         new FakeDisplay());
-        put(IFormFactory.class,     new FakeFormFactory());
+        put(ILogManager.class,                new XLogManager());
+        put(XLogWriter.class,                 new XLogWriter());
+        put(ServiceProvider.class,            ServiceProvider.NULL);
+        put(IDisplay.class,                   new FakeDisplay());
+        put(IFormFactory.class,               new FakeFormFactory());
+        put(ItemListPageFactoryFactory.class, new SEItemListPageFactoryFactory());
     }
     
     static void put(Class clazz, Object object) {
