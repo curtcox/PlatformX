6701a299e4979a1b0f6bb2b5b3ce418e3c86fffb
Curt Cox <curtcox@gmail.com>
2015-07-03 13:05:29 -0500

    Deleted debugging class.

--- OC1/test/config/ShouldRun.java
+++ OC1/test/config/ShouldRun.java
@@ -2,14 +2,17 @@ package config;
 
 public class ShouldRun {
 
-    public static final boolean CodenameOne = false;
-    public static final boolean RoboVM = false;
-    public static final boolean Android = false;
-    public static final boolean JavaSE = false;
+    // Set to run all tests
+    private static final boolean All = false;
 
-    public static final boolean X = false;
-    public static final boolean Google = false;
-    public static final boolean Hash = false;
-    public static final boolean Mite = false;
-    public static final boolean Mach = false;
+    public static final boolean CodenameOne = All || false;
+    public static final boolean      RoboVM = All || false;
+    public static final boolean     Android = All || false;
+    public static final boolean      JavaSE = All || false;
+
+    public static final boolean           X = All || false;
+    public static final boolean      Google = All || false;
+    public static final boolean        Hash = All || false;
+    public static final boolean        Mite = All || false;
+    public static final boolean        Mach = All || false;
 }
