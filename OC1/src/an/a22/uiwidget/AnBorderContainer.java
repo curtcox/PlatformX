9d60a954d0ebbb4f61cec099a025123d8c894c97
Curt Cox <curtcox@gmail.com>
2015-07-14 09:46:15 -0500

    Creating Android BorderContainer

--- OC1/src/an/a22/uiwidget/AnBorderContainer.java
+++ OC1/src/an/a22/uiwidget/AnBorderContainer.java
@@ -8,23 +8,26 @@ import x.Registry;
 public final class AnBorderContainer
     extends RelativeLayout
 {
-    private AnBorderContainer(Context context) {
+    private AnBorderContainer(View center, Context context) {
         super(context);
+        addView(center);
     }
 
-    public static AnBorderContainer of(View view) {
-        return new AnBorderContainer(context());
+    public static AnBorderContainer of(View center) {
+        return new AnBorderContainer(center,context());
     }
 
     private static Context context() {
         return Registry.get(Context.class);
     }
 
-    public AnBorderContainer addNorth(View component) {
+    public AnBorderContainer addNorth(View north) {
+        addView(north);
         return this;
     }
 
-    public AnBorderContainer addEast(View component) {
+    public AnBorderContainer addEast(View east) {
+        addView(east);
         return this;
     }
 }
---
+++ OC1/test/an/a22/uiwidget/AnBorderContainerTest.java
@@ -0,0 +1,75 @@
+package an.a22.uiwidget;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import config.ShouldRun;
+import fake.FakeAnRegistryLoader;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import x.Registry;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest=Config.NONE)
+public class AnBorderContainerTest {
+
+    View center;
+    AnBorderContainer testObject;
+
+    @Before
+    public void setUp() {
+        assumeTrue(ShouldRun.Android);
+        FakeAnRegistryLoader.load();
+        center = view();
+        testObject = AnBorderContainer.of(center);
+    }
+
+    @Test
+    public void is_RelativeLayout() {
+        assertTrue(testObject instanceof RelativeLayout);
+    }
+
+    @Test
+    public void center_is_a_child_view() {
+        assertTrue(testObject.getChildCount()==1);
+        assertSame(center,testObject.getChildAt(0));
+    }
+
+    @Test
+    public void adding_child_views_can_be_chained() {
+        assertSame(testObject,testObject.addNorth(view()));
+        assertSame(testObject,testObject.addEast(view()));
+    }
+
+    @Test
+    public void addNorth_adds_the_view_as_a_child_view() {
+        View north = view();
+        testObject.addNorth(north);
+        assertTrue(testObject.getChildCount() == 2);
+        assertSame(north,testObject.getChildAt(1));
+    }
+
+    @Test
+    public void addEast_adds_the_view_as_a_child_view() {
+        View east = view();
+        testObject.addEast(east);
+        assertTrue(testObject.getChildCount()==2);
+        assertSame(east,testObject.getChildAt(1));
+    }
+
+    private View view() {
+        return new TextView(context());
+    }
+
+    private static Context context() {
+        return Registry.get(Context.class);
+    }
+}
