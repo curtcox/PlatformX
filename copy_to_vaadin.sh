26c7bc87dc774f71cd94a3b76309a88c87e65753
Curt Cox <curtcox@gmail.com>
2015-08-05 17:46:31 -0500

    Added init version of vaadin script

---
+++ copy_to_vaadin.sh
@@ -0,0 +1,9 @@
+rm -r vaadin/app/src/main/java/
+mkdir -p vaadin/app/src/main/java/va
+mkdir -p vaadin/app/src/main/java/x
+mkdir -p vaadin/app/src/main/java/hash
+mkdir -p vaadin/app/src/main/java/google
+cp -R OC1/src/va vaadin/app/src/main/java/
+cp -R OC1/src/x vaadin/app/src/main/java/
+cp -R OC1/src/hash vaadin/app/src/main/java/
+cp -R OC1/src/google vaadin/app/src/main/java/
