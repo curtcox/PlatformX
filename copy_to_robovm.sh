41c83af75cecb1587e737ee3eab18b2d48785ade
Curt Cox <curtcox@gmail.com>
2015-08-14 18:07:24 -0500

    Initial version of copy_to_robovm.sh

---
+++ copy_to_robovm.sh
@@ -0,0 +1,9 @@
+rm -r RoboVM/src/main/java/
+mkdir -p RoboVM/src/main/java/ios
+mkdir -p RoboVM/src/main/java/x
+mkdir -p RoboVM/src/main/java/hash
+mkdir -p RoboVM/src/main/java/google
+cp -R OC1/src/ios RoboVM/src/main/java/
+cp -R OC1/src/x RoboVM/src/main/java/
+cp -R OC1/src/hash RoboVM/src/main/java/
+cp -R OC1/src/google RoboVM/src/main/java/
