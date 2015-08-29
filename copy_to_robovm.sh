0abb6cb5bf927bbbaef279dfe32f152eeafe1251
Curt Cox <curtcox@gmail.com>
2015-08-28 19:42:55 -0500

    Added script purpose comments

--- copy_to_robovm.sh
+++ copy_to_robovm.sh
@@ -1,3 +1,6 @@
+# Copy the files need by the iOS app from the shared project, to the
+# iOS project.
+
 rm -r RoboVM/src/main/java/
 mkdir -p RoboVM/src/main/java/ios
 mkdir -p RoboVM/src/main/java/x
@@ -7,3 +10,7 @@ cp -R OC1/src/ios RoboVM/src/main/java/
 cp -R OC1/src/x RoboVM/src/main/java/
 cp -R OC1/src/hash RoboVM/src/main/java/
 cp -R OC1/src/google RoboVM/src/main/java/
+
+# The follow line copies any changes made directly in the iOS project,
+# back to the shared project.
+# cp -R  RoboVM/src/main/java/ OC1/src/
