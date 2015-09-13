30adc652368da8b98b18ce2686f18a39100fb71c
Curt Cox <curtcox@gmail.com>
2015-09-13 07:40:18 -0500

    Added tests

--- OC1/test/x/page/MatchingIndexedPagesCompositePageFactoryTest.java
+++ OC1/test/x/page/MatchingIndexedPagesCompositePageFactoryTest.java
@@ -4,6 +4,8 @@ import mach.Mocks;
 import org.junit.Before;
 import org.junit.Test;
 import x.Registry;
+import x.log.ILog;
+import x.log.ILogManager;
 import x.pagefactories.ItemListPageFactoryFactory;
 import x.uiwidget.XComponent;
 
@@ -31,24 +33,106 @@ public class MatchingIndexedPagesCompositePageFactoryTest {
     ItemListPageFactoryFactory itemListPageFactoryFactory;
     PageFactory itemListPageFactory;
     MatchingIndexedPagesCompositePageFactory factory;
+
+    Page page1 = new Page(link) {
+        @Override
+        public XComponent layoutForPortrait() {
+            return null;
+        }
+    };
+    Page page2 = new Page(link) {
+        @Override
+        public XComponent layoutForPortrait() {
+            return null;
+        }
+    };
+    PageFactory factory1;
+    PageFactory factory2;
     PageFactory inner;
+    ILogManager logManager;
+    ILog log;
 
     @Before
     public void setUp() {
         Mocks.init(this);
+        _(log); logManager.getLog(AllMatchingPagesCompositePageFactory.class);
         Registry.put(ItemListPageFactoryFactory.class, itemListPageFactoryFactory);
-
+        Registry.put(ILogManager.class,logManager);
         factory = new MatchingIndexedPagesCompositePageFactory(inner);
     }
 
     @Test
     public void can_create() {
-        assertNotNull(new IndexPageCompositePageFactory(inner));
+        assertNotNull(new MatchingIndexedPagesCompositePageFactory());
+        assertNotNull(new MatchingIndexedPagesCompositePageFactory(factory1));
+        assertNotNull(new MatchingIndexedPagesCompositePageFactory(factory1,factory2));
     }
 
     @Test
+    public void create_return_an_empty_array_when_there_are_no_factories() {
+        PageFactory factory = new AllMatchingPagesCompositePageFactory();
+
+        Page[] actual = factory.create(link);
+
+        assertEquals(0, actual.length);
+    }
+
+    @Test
+    public void create_returns_page_from_only_factory_when_it_matches() {
+        PageFactory factory = new AllMatchingPagesCompositePageFactory(factory1);
+
+        _(new Page[] {page1}); factory1.create(link);
+
+        Page[] actual = factory.create(link);
+
+        assertEquals(1,actual.length);
+        assertSame(page1,actual[0]);
+    }
+
+    @Test
+    public void create_returns_page_from_1st_factory_when_it_is_the_only_one_that_matches() {
+        PageFactory factory = new AllMatchingPagesCompositePageFactory(factory1,factory2);
+
+        _(new Page[]{page1}); factory1.create(link);
+        _(new Page[0]);        factory2.create(link);
+
+        Page[] actual = factory.create(link);
+
+        assertEquals(1, actual.length);
+        assertSame(page1,actual[0]);
+    }
+
+    @Test
+    public void create_returns_page_from_2nd_factory_when_it_is_the_only_one_that_matches() {
+        PageFactory factory = new AllMatchingPagesCompositePageFactory(factory1,factory2);
+
+        _(new Page[0]); factory1.create(link);
+        _(new Page[] {page1}); factory2.create(link);
+
+        Page[] actual = factory.create(link);
+
+        assertEquals(1, actual.length);
+        assertSame(page1,actual[0]);
+    }
+
+    @Test
+    public void create_returns_page_from_both_factories_when_they_match() {
+        PageFactory factory = new AllMatchingPagesCompositePageFactory(factory1,factory2);
+
+        _(new Page[] {page1}); factory1.create(link);
+        _(new Page[] {page2}); factory2.create(link);
+
+        Page[] actual = factory.create(link);
+
+        assertEquals(2,actual.length);
+        assertSame(page1, actual[0]);
+        assertSame(page2, actual[1]);
+    }
+
+
+    @Test
     public void is_a_PageFactory() {
-        assertTrue(new IndexPageCompositePageFactory(inner) instanceof PageFactory);
+        assertTrue(new MatchingIndexedPagesCompositePageFactory(inner) instanceof PageFactory);
     }
 
     @Test
