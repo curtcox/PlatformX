2ed66ebaa74ddfe1db28cf06d9a2155eb3fd0395
Curt Cox <curtcox@gmail.com>
2015-09-20 18:16:43 -0500

    Added SESwappable List Factory

---
+++ OC1/src/se/event/SESwappableListFactory.java
@@ -0,0 +1,14 @@
+package se.event;
+
+import x.event.SwappableList;
+
+import java.util.List;
+
+public class SESwappableListFactory
+    implements SwappableList.Factory
+{
+    @Override
+    public SwappableList from(List list) {
+        return null;
+    }
+}
