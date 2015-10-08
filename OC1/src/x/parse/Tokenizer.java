df6c6b4e57d72c7380ca4aa9c89b916ed0285487
Curt Cox <curtcox@gmail.com>
2015-10-08 17:47:17 -0500

    Rewrote tokenizer to not use recursion

--- OC1/src/x/parse/Tokenizer.java
+++ OC1/src/x/parse/Tokenizer.java
@@ -1,6 +1,6 @@
 package x.parse;
 
-import x.util.Strings;
+import java.util.*;
 
 /**
  * For splitting a string into tokens.
@@ -21,11 +21,28 @@ public final class Tokenizer {
      */
     public static String[] tokenize(String string,String... separators) {
         checkSeparators(separators);
-        int at = startOfFirst(string,separators);
-        if (at<0) {
-            return (string.length()==0) ? new String[0] : new String[] {string};
+        List<String> tokens = new ArrayList<String>();
+        StringBuilder token = null;
+        Set<String> set = new HashSet<String>(Arrays.asList(separators));
+        for (int i=0; i<string.length(); i++) {
+            String c = string.substring(i,i+1);
+            if (set.contains(c)) {
+                if (token!=null) {
+                    tokens.add(token.toString());
+                    token = null;
+                }
+                tokens.add(c);
+            } else {
+                if (token==null) {
+                    token = new StringBuilder();
+                }
+                token.append(c);
+            }
         }
-        return join(before(string,at),at(string,at),tokenize(after(string,at),separators));
+        if (token!=null) {
+            tokens.add(token.toString());
+        }
+        return tokens.toArray(new String[0]);
     }
 
     private static void checkSeparators(String[] separators) {
@@ -35,61 +52,6 @@ public final class Tokenizer {
                 throw new IllegalArgumentException(message);
             }
         }
-    }
-
-    private static int startOfFirst(String string, String... tokens) {
-        int min = Integer.MAX_VALUE;
-        for (String token : tokens) {
-            int at = string.indexOf(token);
-            if (at>=0 && at<min) {
-                min = at;
-            }
-        }
-        return (min == Integer.MAX_VALUE) ? -1 : min;
-    }
-    
-    private static String before(String string,int at) {
-        return string.substring(0,at);
-    }
-
-    private static String at(String string,int at) {
-        return string.substring(at, at + 1);
-    }
-
-    private static String after(String string,int at) {
-        return string.substring(at+1, string.length());
-    }
-
-    private static String[] join(String head, String node, String[] tail) {
-        if (head.length()>0 && node.length()>0) {
-            return joinHeadNodeTail(head,node,tail);
-        }
-        if (head.length()>0) {
-            return join(head,tail);
-        }
-        if (node.length()>0) {
-            return join(node,tail);
-        }
-        return tail;
-    }
-
-    private static String[] joinHeadNodeTail(String head, String node, String[] tail) {
-        String[] all = new String[tail.length + 2];
-        all[0] = head;
-        all[1] = node;
-        for (int i=0; i<tail.length; i++) {
-            all[i+2] = tail[i];
-        }
-        return all;
-    }
-
-    private static String[] join(String head, String[] tail) {
-        String[] all = new String[tail.length + 1];
-        all[0] = head;
-        for (int i=0; i<tail.length; i++) {
-            all[i+1] = tail[i];
-        }
-        return all;
     }
 
 }
